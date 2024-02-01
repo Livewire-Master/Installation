b9f5657e79c896d8c1abd90b3c648070ca0fa8f3
RealMrHex <RealMrHex@gmail.com>
2024-02-02 02:24:09 +0330

    configuring Livewire's update endpoint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Livewire::setUpdateRoute(function ($handle)
+        {
+            return Route::post('ajax/dispatcher', $handle);
+        });
     }
 }
